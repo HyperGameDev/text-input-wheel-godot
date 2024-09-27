2ffca170d52195f2e51f29e4f7882846998d25f0
ocean <ocean@ocean.lol>
2024-09-27 21:31:56 +0100

    display picked theme

--- addons/spin_wheel.gd
+++ addons/spin_wheel.gd
@@ -14,45 +14,54 @@ var vat_pham: Array[Dictionary] = [
         "name": "Dark blue",
         "from": 0,
         "to": 45,
-    },
-    {
-        "name": "Dark green",
-        "from": 45,
-        "to": 90,
-    },
-    {
-        "name": "Blue",
-        "from": 90,
-        "to": 135,
-    },
-    {
-        "name": "Yellow",
-        "from": 135,
-        "to": 180,
-    },
-    {
-        "name": "Purple",
-        "from": 180,
-        "to": 225,
-    },
-    {
-        "name": "Green",
-        "from": 225,
-        "to": 270,
-    },
-    {
-        "name": "Orange",
-        "from": 270,
-        "to": 315,
+        "color": Color.BLUE
     },
     {
         "name": "Pink",
         "from": 315,
         "to": 360,
+        "color": Color.MAGENTA
+    },
+    {
+        "name": "Orange",
+        "from": 270,
+        "to": 315,
+        "color": Color.ORANGE
+    },
+    {
+        "name": "Green",
+        "from": 225,
+        "to": 270,
+        "color": Color.GREEN
+    },
+    {
+        "name": "Purple",
+        "from": 180,
+        "to": 225,
+        "color": Color.PURPLE
+    },
+    {
+        "name": "Yellow",
+        "from": 135,
+        "to": 180,
+        "color": Color.YELLOW
+    },
+    {
+        "name": "Blue",
+        "from": 90,
+        "to": 135,
+        "color": Color.CYAN
+    },
+    {
+        "name": "Dark green",
+        "from": 45,
+        "to": 90,
+        "color": Color.DARK_GREEN
     }
 ]
 
 func _ready() -> void:
+    %Panel.get_parent().position.y = %Panel.size.y
     var text_container: Control = text_box.get_parent()
     for i in 8:
         var new_text_box: LineEdit = text_box.duplicate()
@@ -61,6 +70,7 @@ func _ready() -> void:
         new_text_box.pivot_offset = new_text_box.size / 2
         new_text_box.position = (Vector2(0, -331)).rotated(new_text_box.rotation) - new_text_box.size / 2
         text_boxes.append(new_text_box)
+        vat_pham[i].text_box = new_text_box
         new_text_box.connect("focus_entered", func():
             var new_target = -new_text_box.rotation_degrees * TAU / 360
             if target_rot != new_target:
@@ -83,20 +93,22 @@ func _on_btn_spin_pressed():
     if is_spin == false:
         is_spin = true
         var tween := get_tree().create_tween().set_parallel()
-        tween.connect("finished", func():
-            #after tween finish animation, this function is call
-            var old_rotation_degrees = %front.rotation_degrees
-            #set is_spin = false to tell for user can press again
-            is_spin = false
-            if old_rotation_degrees > 360:
-                #This part is to fix the error that when rotating the steamer once, it will not rotate counterclockwise
-                var rad_ = fmod(old_rotation_degrees, 360)
-                %front.rotation_degrees = rad_
-            )
         reward_position = randi_range(0, 360) #random position from 0 to 360 degrees
 
+        var selected: Dictionary
         for item in vat_pham:
             if reward_position >= item.from - 22.5 and reward_position <= item.to - 22.5:
-                print(item.name)
-                #signal for another scene
+                selected = item
+        tween.tween_property(%Panel.get_parent(), "position:y", %Panel.size.y, 0.5)
         tween.tween_property(%front, "rotation_degrees", reward_position +  360 * speed * power , 3).set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_CIRC)
+        await tween.finished
+        %Theme.text = selected.text_box.text
+        (%Panel as Panel).get_theme_stylebox("panel").bg_color = selected.color
+        var old_rotation_degrees = %front.rotation_degrees
+        #set is_spin = false to tell for user can press again
+        is_spin = false
+        %front.rotation_degrees = fmod(old_rotation_degrees, 360)
+        tween = get_tree().create_tween()
+        tween.set_ease(Tween.EASE_IN_OUT)
+        tween.set_trans(Tween.TRANS_QUAD)
+        tween.tween_property(%Panel.get_parent(), "position:y", 0, 1.0)
