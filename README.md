e1b063d5cb06339a3adc601440469730d241f63b
Lýz <147095077+HyperGameDev@users.noreply.github.com>
2025-02-10 22:37:20 -0600

    Added coin flip mode
    data.json not working I think

--- README.md
+++ README.md
@@ -6,12 +6,12 @@ A spinnable wheel with text inputs, history-keeping, topics display, and more.
 
 <table style="border: none; border-collapse: collapse;">
   <tr style="border: none;">
-    <td style="border: none; padding-right: 10px;">
-      <img src="https://github.com/HyperGameDev/text-input-wheel-godot/blob/main/screenshots/wheel1.png" alt="Text input wheel spun and landed on a topic">
-    </td>
-    <td style="border: none; padding-left: 10px;">
-      <img src="https://github.com/HyperGameDev/text-input-wheel-godot/blob/main/screenshots/wheel2.png" alt="Text input wheel with viewable history of topics">
-    </td>
+	<td style="border: none; padding-right: 10px;">
+	  <img src="https://github.com/HyperGameDev/text-input-wheel-godot/blob/main/screenshots/wheel1.png" alt="Text input wheel spun and landed on a topic">
+	</td>
+	<td style="border: none; padding-left: 10px;">
+	  <img src="https://github.com/HyperGameDev/text-input-wheel-godot/blob/main/screenshots/wheel2.png" alt="Text input wheel with viewable history of topics">
+	</td>
   </tr>
 </table>
 
--- addons/spin_wheel.gd
+++ addons/spin_wheel.gd
@@ -1,68 +1,88 @@
 extends Control
 
-@export var is_spin: bool = false
+
+@export var coin_flip_mode: bool = false
+var heads: bool
+var is_spin: bool = false
 @export var speed: int = 10
-@export var power: int = 2
+@export_range(-2,2) var power: float = 2
 @export var reward_position: float = 0
 @export var text_box: LineEdit
+@export var coin_icon: TextureRect
 var focused_text_box: LineEdit
 var text_boxes: Array[LineEdit] = []
 var target_rot: float = 0
 var from_rot: float
 
-var save_path: String = "res://data.json"
+var save_path: String = "res://export/data.json"
+
 
 @onready var history_label: Control = %History_Label
 @onready var history_animations: AnimationPlayer = $History/Panel_History/HistoryAnimations
+@onready var wheel: TextureRect = %wheel_outline
+@onready var arrow: TextureRect = %arrow
+
+@onready var animation: AnimationPlayer = %AnimationPlayer
+@onready var coin: MeshInstance3D = $SubViewport/coin_mesh/Cylinder
+@onready var coin_flip_minigame: Control = %Coin_Flip_Minigame
+
 
 var vat_pham: Array[Dictionary] = [
 	{
 		"name": "Dark blue",
 		"from": 0,
 		"to": 45,
-		"color": Color(0.0,0.329,1.0)
+		"color": Color(0.0,0.329,1.0),
+		"has_coin": false
 	},
 	{
 		"name": "Pink",
 		"from": 315,
 		"to": 360,
-		"color": Color(0.871,0.071,0.412)
+		"color": Color(0.871,0.071,0.412),
+		"has_coin": false
 	},
 	{
 		"name": "Orange",
 		"from": 270,
 		"to": 315,
-		"color": Color(1.0,0.467,0.016)
+		"color": Color(1.0,0.467,0.016),
+		"has_coin": false
 	},
 	{
 		"name": "Green",
 		"from": 225,
 		"to": 270,
-		"color": Color(0.084,0.749,0.098)
+		"color": Color(0.084,0.749,0.098),
+		"has_coin": false
 	},
 	{
 		"name": "Purple",
 		"from": 180,
 		"to": 225,
-		"color": Color(0.624,0.039,0.624)
+		"color": Color(0.624,0.039,0.624),
+		"has_coin": false
 	},
 	{
 		"name": "Yellow",
 		"from": 135,
 		"to": 180,
-		"color": Color(1.0,0.765,0.043)
+		"color": Color(1.0,0.765,0.043),
+		"has_coin": false
 	},
 	{
 		"name": "Blue",
 		"from": 90,
 		"to": 135,
-		"color": Color(0.008,0.675,1.0)
+		"color": Color(0.008,0.675,1.0),
+		"has_coin": false
 	},
 	{
 		"name": "Dark green",
 		"from": 45,
 		"to": 90,
-		"color": Color(0.075,0.596,0.027)
+		"color": Color(0.075,0.596,0.027),
+		"has_coin": false
 	}
 ]
 
@@ -71,14 +91,17 @@ var history : Dictionary = {} #Structured as {"theme_name", amount_on_wheel}
 func _ready() -> void:
 	%History_Label.meta_clicked.connect(_on_history_url_clicked)
 	%Panel.get_parent().position.y = %Panel.size.y
-	var text_container: Control = text_box.get_parent()
 	history = load_history(save_path)
 	for i in 8:
 		var new_text_box: LineEdit = text_box.duplicate()
-		text_container.add_child(new_text_box)
-		new_text_box.rotation_degrees = 45 * i
-		new_text_box.pivot_offset = new_text_box.size / 2
-		new_text_box.position = (Vector2(0, -331)).rotated(new_text_box.rotation) - new_text_box.size / 2
+		var new_coin_icon: TextureRect = coin_icon.duplicate()
+		add_slice_element(i,new_text_box,0,0)
+		
+		if i % 2 == 0 and coin_flip_mode:
+			add_slice_element(i,new_coin_icon,-8,100)
+			vat_pham[i].has_coin = true
+			
+
 		text_boxes.append(new_text_box)
 		vat_pham[i].text_box = new_text_box
 		new_text_box.connect("focus_entered", func():
@@ -98,7 +121,15 @@ func _ready() -> void:
 			new_text_box.pivot_offset = new_text_box.size / 2
 		)
 	text_box.queue_free()
+	coin_icon.queue_free()
 	text_boxes[0].grab_focus()
+	
+func add_slice_element(slice_id,new_element,x_offset,y_offset):
+	var text_container: Control = text_box.get_parent()
+	text_container.add_child(new_element)
+	new_element.rotation_degrees = 45 * slice_id
+	new_element.pivot_offset = new_element.size / 2
+	new_element.position = (Vector2(0 + x_offset, -331 + y_offset)).rotated(new_element.rotation) - new_element.size / 2
 
 func _on_btn_spin_pressed():
 	if is_spin == false:
@@ -114,20 +145,23 @@ func _on_btn_spin_pressed():
 		tween.tween_property(%Panel.get_parent(), "position:y", %Panel.size.y, 0.5)
 		tween.tween_property(%front, "rotation_degrees", reward_position +  360 * speed * power , 3).set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_CIRC)
 		await tween.finished
+		#print("Wheel spun! On: ",selected)
 		var current_choice = selected.text_box.text
 		%Theme.text = current_choice
-		purge_history(current_choice)
-		(%Panel as Panel).get_theme_stylebox("panel").bg_color = selected.color
-		#(%Panel as Panel).get_material().set_shader_parameter("color_one",selected.color)
-		(%Theme as Label).add_theme_color_override("font_outline_color", selected.color)
+		
+		assign_banner_colors(selected)
+		check_if_has_coin(selected,current_choice)
 		var old_rotation_degrees = %front.rotation_degrees
 		#set is_spin = false to tell for user can press again
 		is_spin = false
 		%front.rotation_degrees = fmod(old_rotation_degrees, 360)
-		tween = get_tree().create_tween()
-		tween.set_ease(Tween.EASE_IN_OUT)
-		tween.set_trans(Tween.TRANS_QUAD)
-		tween.tween_property(%Panel.get_parent(), "position:y", 0, 1.0)
+		if selected.has_coin:
+			show_coin_flip()
+		else:
+			show_banner()
+			
+		if heads or not coin_flip_mode:
+			purge_history(current_choice)
 
 ## HISTORY CODE
 
@@ -175,6 +209,57 @@ func update_history_label(history: Dictionary):
 	%History_Label.bbcode_enabled = true
 	%History_Label.text = str
 	history_label.text = str
+	
+func assign_banner_colors(selected):
+		(%Panel as Panel).get_theme_stylebox("panel").bg_color = selected.color
+		#(%Panel as Panel).get_material().set_shader_parameter("color_one",selected.color)
+		(%Theme as Label).add_theme_color_override("font_outline_color", selected.color)
+		
+func check_if_has_coin(selected,current):
+	if selected.has_coin:
+		print(current," has a coin!")
+		
+func show_banner():
+	var banner_tween = get_tree().create_tween()
+	banner_tween.set_ease(Tween.EASE_IN_OUT)
+	banner_tween.set_trans(Tween.TRANS_QUAD)
+	banner_tween.tween_property(%Panel.get_parent(), "position:y", 0, 1.0)
+	
+func show_coin_flip():
+	modulate_wheel_alpha(.255,true)
+	
+func _animation_coin_entered():
+	animation.play("flip")
+
+func _animation_coin_flipped():
+	if not coin_flip_is_heads():
+		var tails_mat: StandardMaterial3D = preload("res://addons/coin_mesh/tails.tres")
+		coin.set_surface_override_material(1,tails_mat)
+	
+func coin_flip_is_heads() -> bool:
+	heads = randi() % 2
+	return heads
+	
+func _animation_coin_side_revealed():
+	animation.play("idle")
+	if heads:
+		show_banner()
+
+func modulate_wheel_alpha(alpha,flip_coin):
+	var overlay_tween = get_tree().create_tween()
+	overlay_tween.set_ease(Tween.EASE_IN_OUT)
+	overlay_tween.set_trans(Tween.TRANS_QUAD)
+	overlay_tween.tween_property(arrow, "modulate:a", alpha, 1.0)
+	overlay_tween.set_parallel()
+	overlay_tween.tween_property(wheel, "modulate:a", alpha, 1.0)
+	
+	if flip_coin:
+		await overlay_tween.finished
+	
+		coin_flip_minigame.visible = true
+		animation.play("coin_enter")
+	
+	
 
 func _on_history_button_toggled(toggled_on: bool) -> void:
 	if toggled_on:
